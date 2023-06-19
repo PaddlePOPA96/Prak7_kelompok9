99cecd9d6ac33b289cb07b51476ae033e4329541
PaddlePOPA96 <dutahad@gmail.com>
2023-06-19 23:47:52 +0700

    inheritage view

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $pageTitle }}</title>
-    @vite('resources/sass/app.scss')
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-        <div class="container">
-            <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Data
-                Master</a>
-
-            <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <hr class="d-lg-none text-white-50">
-
-                <ul class="navbar-nav flex-row flex-wrap">
-                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
-                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('employees.index') }}"
-                            class="nav-link active">Employee List</a></li>
-                </ul>
-
-                <hr class="d-lg-none text-white-50">
-
-                <a href="{{ route('profile') }}" class="btn btn-outline-light my-2 ms-md-auto"><i
-                        class="bi-person-circle me-1"></i> My Profile</a>
-            </div>
-        </div>
-    </nav>
-
+@section('content')
     <div class="container mt-4">
         <div class="row mb-0">
             <div class="col-lg-9 col-xl-10">
@@ -68,18 +32,17 @@
                             <td>{{ $employee->lastname }}</td>
                             <td>{{ $employee->email }}</td>
                             <td>{{ $employee->age }}</td>
-                            <td>{{ $employee->position_name }}</td>
+                            <td>{{ $employee->position->name }}</td>
                             <td>
+                                {{-- ACTIONS SECTION --}}
                                 <div class="d-flex">
-                                    <a href="{{ route('employees.show', ['employee' => $employee->employee_id]) }}"
-                                        class="btn btn-outline-dark btn-sm me-2"><i
-                                            class="bi-person-lines-fill"></i></a>
-                                    <a href="{{ route('employees.edit', ['employee' => $employee->employee_id]) }}"
+                                    <a href="{{ route('employees.show', ['employee' => $employee->id]) }}"
+                                        class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                    <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}"
                                         class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                     <div>
-                                        <form
-                                            action="{{ route('employees.destroy', ['employee' => $employee->employee_id]) }}"
+                                        <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}"
                                             method="POST">
                                             @csrf
                                             @method('delete')
@@ -95,8 +58,4 @@
             </table>
         </div>
     </div>
-
-    @vite('resources/js/app.js')
-</body>
-
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
-                <a class="btn btn-dark" href="{{url('home')}}">Home</a>
+                <a class="btn btn-dark" href="{{route('home')}}">Home</a>
             </div>
         </div>
     </div>
